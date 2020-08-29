be23a74aec1d7f1e8ff9febfd8ecb9f9161d0af7
benibana2001 <benibana2001@gmail.com>
2020-08-29 21:32:15 +0900

    [three] to be functionalized

--- threejs/src/index.js
+++ threejs/src/index.js
@@ -1,29 +1,98 @@
 import * as THREE from "three";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-const renderer = new THREE.WebGLRenderer();
+function main() {
+  // set renderer
+  const renderer = new THREE.WebGLRenderer();
+  document.body.appendChild(renderer.domElement);
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  // set camera
+  const fov = 40;
+  const aspect = 2;
+  const near = 0.1;
+  const far = 1000;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+  camera.position.z = 120;
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  // set scene
+  const scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xaaaaaa);
+  const objects = [];
+  const spread = 15;
 
-camera.position.z = 5;
+  // add light
+  {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    scene.add(light);
+  }
 
-function animate() {
+  {
+    const boxWidth = 30;
+    const boxHeight = 30;
+    const boxDepth = 30;
+    addSolidGeometry(
+      0,
+      0,
+      new THREE.BoxBufferGeometry(boxWidth, boxHeight, boxDepth)
+    );
+  }
+
+  function addObject(x, y, obj) {
+    obj.position.x = x * spread;
+    obj.position.y = y * spread;
+
+    scene.add(obj);
+    objects.push(obj);
+  }
+
+  function createMaterial() {
+    //  This tells three to draw both sides of the triangles that make up a shape.
+    const material = new THREE.MeshPhongMaterial({
+      side: THREE.DoubleSide,
+    });
+
+    const hue = Math.random();
+    const saturation = 1;
+    const luminance = 0.5;
+    material.color.setHSL(hue, saturation, luminance);
+
+    return material;
+  }
+
+  function addSolidGeometry(x, y, geometry) {
+    const mesh = new THREE.Mesh(geometry, createMaterial());
+    addObject(x, y, mesh);
+  }
+
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement;
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
+    const needResize = canvas.width !== width || canvas.height !== height;
+    if (needResize) {
+      renderer.setSize(width, height, false);
+    }
+    return needResize;
+  }
+
+  function animate() {
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    objects.forEach((obj) => {
+      obj.rotation.x += 0.01;
+      obj.rotation.y += 0.01;
+      renderer.render(scene, camera);
+    });
+    requestAnimationFrame(animate);
+  }
+
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
 }
 
-animate();
+main();
